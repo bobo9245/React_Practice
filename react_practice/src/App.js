2f5238e7c2d58c92681418da1a1414ab6ceed532
서윤하 <bobo9245@naver.com>
2022-09-04 08:31:10 +0000

    220904-React 기본세팅

---
+++ react_practice/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='SassComponent'>
+		<div className='box red'></div>
+		<div className='box red'></div>
+		<div className='box red'></div>
+		<div className='box red'></div>
+		<div className='box red'></div>
+		<div className='box red'></div>
+		<div className='box red'></div>
+	</div>
+  );
+}
+
+export default App;
